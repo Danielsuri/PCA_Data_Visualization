c32360a3164dedae33456d54901b44d37583cd05
Daniel Surizon <danielsuri@gmail.com>
2019-06-13 18:01:17 +0300

    playing with pandas

--- main.py
+++ main.py
@@ -22,40 +22,39 @@ all_pca_data_T.columns = all_pca_data_T.iloc[0]
 features = list(all_pca_data_T.index.values)
 # del features[0]
 for idx, items in enumerate(features[1::]):
-    features[idx+1] = items.split('_')[0]
-all_pca_data_T.rename(index=features)
-
-x = all_pca_data_T.loc[:, data_idxes].values
-
-headers = list(all_pca_data.columns)
-del headers[0]
-
-# y = list(all_pca_data.columns)
-# del y[0]
-# for idx, items in enumerate(y):
-#     y[idx] = items.split('_')[0]
-
-x = all_pca_data.loc[:, headers].values
+    features[idx + 1] = items.split('_')[0]
+featDf = pd.DataFrame([features])
+featDf.drop(columns=0, inplace=True)
+featDf = featDf.T
+featDf.reset_index(inplace=True)
+featDf.drop(columns='index', inplace=True)
+x = all_pca_data_T.iloc[1:].values
+y = all_pca_data_T.iloc[1:].index.values
 x = StandardScaler().fit_transform(x)
+df = pd.DataFrame(data=x, index=features[1::])
+
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data=principalComponents
                            , columns=['principal component 1', 'principal component 2'])
-finalDf = pd.concat([principalDf, y], axis=1)
-# fig = plt.figure(figsize=(8, 8))
-# ax = fig.add_subplot(1, 1, 1)
-# ax.set_xlabel('Principal Component 1', fontsize=15)
-# ax.set_ylabel('Principal Component 2', fontsize=15)
-# ax.set_title('2 Component PCA', fontsize=20)
-# targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-# colors = ['r', 'g', 'b']
-# for target, color in zip(targets, colors):
-#     indicesToKeep = finalDf['target'] == target
-#     ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-#                , finalDf.loc[indicesToKeep, 'principal component 2']
-#                , c=color
-#                , s=50)
-# ax.legend(targets)
-# ax.grid()
 
-# #########################################
+finalDf = pd.concat([principalDf, featDf], axis=1)
+finalDf.rename(columns={0: 'target'}, inplace=True)
+
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel('Principal Component 1', fontsize=15)
+ax.set_ylabel('Principal Component 2', fontsize=15)
+ax.set_title('2 Component PCA', fontsize=20)
+targets = ['Air', 'Egg', 'Yogurt']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets, colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c=color
+               , s=50)
+ax.legend(targets)
+ax.grid()
+del (idx, items, all_files)
