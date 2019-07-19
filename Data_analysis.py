67bdd6c67f6e0518d7252b930748b1918ce75dd5
Daniel Surizon <danielsuri@gmail.com>
2019-07-19 14:54:49 +0300

    absorbance calculation

--- Data_analysis.py
+++ Data_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import glob
+from scipy.signal import savgol_filter
 
 all_files = glob.glob('All_samples/*.txt')
 all_files.sort()
@@ -16,6 +17,25 @@ for file_name in all_files:
 df = df.T
 df.columns = data['Wave_Length']
 df = df.T
+# Get dark values and subtract them from all data
+dark = df['Dark_NQ51A06081_21-20-59-000'].values
+df.drop(columns='Dark_NQ51A06081_21-20-59-000', inplace=True)
+df_minus_dark = df.sub(dark, axis='index')
+
+air_sam = []
+str_to_find = 'Air'
+
+sample_names = df.columns.values
+for sample in sample_names:
+    if str_to_find in sample:
+        air_sam.append(sample)
+# Mean over all air samples
+mean_air_sam = df[air_sam].mean(1)
+# Remove all air samples to stay only with measurements data
+df_minus_dark.drop(columns=air_sam, inplace=True)
+# Divide by air mean
+absorbance_df = df_minus_dark.div(mean_air_sam, axis=0)
+
 samples_to_plot = []
 # Choose sample to plot:
 str_to_find = 'COM'
@@ -25,6 +45,8 @@ for sample in sample_names:
     if str_to_find in sample:
         samples_to_plot.append(sample)
 
+x = df.index.values
+y = df[samples_to_plot[0]].values
 df[samples_to_plot].plot(title=str_to_find + ' sample plot')
 
 df[samples_to_plot].diff().plot(title=str_to_find + ' First derivative')
